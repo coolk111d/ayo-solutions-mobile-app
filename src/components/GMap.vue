9b8d640593408dee9253de71f45b50fead5a9fd3
Jerome Altarejos <brojeromealtarejos@gmail.com>
2021-09-17 01:20:51 +0800

    Add Checkout

---
+++ src/components/GMap.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="map" ref="mapDivRef">
+    </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+
+export default {
+    name: "GMap",
+    props: {
+        center: { lat: Number, lng: Number},
+        zoom: Number,
+        mapType: String,
+        disableUI: Boolean,
+    },
+    setup(props) {
+
+        const map = ref(null);
+        const mapDivRef = ref(null);  
+        const currentMarkers = [];
+        onMounted(() => {
+            const key = process.env.VUE_APP_GOOGLEMAPS_KEY;
+
+            const googleMapScript = document.createElement("SCRIPT");
+            googleMapScript.setAttribute(
+                "src",
+                `https://maps.googleapis.com/maps/api/js?key=${key}&callback=initMap`
+            );
+            googleMapScript.setAttribute("defer", "");
+            googleMapScript.setAttribute("async", "");
+            document.head.appendChild(googleMapScript);
+
+        });
+
+
+        const clearMarkers = () => {
+            currentMarkers.forEach(marker => {
+                marker.setMap(null);
+            });
+        }
+
+        window.initMap = () => {
+            map.value = new window.google.maps.Map(mapDivRef.value, {
+                mapTypeId: props.mapType || "hybrid",
+                zoom: props.zoom || 10,
+                disableDefaultUI: props.disableUI || false,
+                center: props.center || { lat: 14.124561213272877, lng: 121.164106030269481}
+            });
+            
+
+            const image = {
+            url: "../assets/images/logo-trans.png", // url
+            scaledSize: new window.google.maps.Size(30, 50), // scaled size
+            origin: new window.google.maps.Point(0,0), // origin
+            anchor: new window.google.maps.Point(15, 50) // anchor
+            };
+
+         map.value.addListener('click', mapEvent =>  {
+                clearMarkers();
+                const newMarker = new window.google.maps.Marker({
+                    position: mapEvent.latLng.toJSON(),
+                    map: map.value,
+                    icon: image
+                });
+                currentMarkers.push(newMarker);
+
+                console.log(mapEvent.latLng.toJSON());
+            });
+        };
+
+
+        return {
+            mapDivRef
+        };
+    },
+
+}
+</script>
+
+<style scoped>
+.map {
+    width: 100%;
+    height: 350px;
+}
+</style>
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -4,73 +4,181 @@
         
         <ion-content :fullscreen="true">
             <h5 style="text-align:center">Checkout</h5>
+           <ion-card>
+                <div class="title-icon">
+                    <ion-icon :icon="mapOutline" class="map"></ion-icon>
+                    <p class="title">Delivery Address</p> 
+                    <ion-icon :icon="pencilOutline" color="warning" class="edit"  @click="addAddress"></ion-icon>
+                </div>
+                <p class="address" @click="addAddress">Blk 20 Lot 21 Mara St. Cabuyao Laguna</p>
 
-            <ion-card>
-                <h4 style="text-align:center">Under Development</h4>
-            </ion-card>
+                <hr>
+                    <ion-item>
+                        <ion-label position="floating" style="font-weight: 600">Delivery Instruction  <ion-icon :icon="bicycleOutline" color="dark" class="edit"  @click="addAddress"></ion-icon></ion-label>
+                        <ion-textarea></ion-textarea>
+                    </ion-item>
+           </ion-card>
 
+           <ion-card>
+                <div class="title-icon">
+                    <ion-icon :icon="walletOutline" class="map"></ion-icon>
+                    <p class="title">Payment Method</p> 
+                </div>
+                    <ion-radio-group>
+                        <ion-item>
+                            
+                            <ion-radio value="Cash"></ion-radio>
+                            <ion-label style="margin-left: 10px">Cash</ion-label>
+                        </ion-item>
 
+                        <ion-item>
+                            <ion-radio value="Debit/Credit" disabled></ion-radio>
+                            <ion-label style="margin-left: 10px">Debit/Credit</ion-label>
+                        </ion-item>
+
+                        <ion-item>
+                            <ion-radio value="GCash/Paymaya" disabled></ion-radio>
+                            <ion-label style="margin-left: 10px">GCash/Paymaya</ion-label>
+                        </ion-item>
+                    </ion-radio-group>
+           </ion-card>
+
+           <ion-card>
+                <div class="title-icon">
+                    <ion-icon :icon="receiptOutline" class="map"></ion-icon>
+                    <p class="title" style="text-align:left; margin-right: 150px;">Summary</p> 
+                </div>
+                <div class="summary-details">
+                    <div class="display-flex">
+                        <p>1x Burger</p>
+                        <p>&#8369; 100.00</p>
+                    </div>
+                    <hr style="margin: 0px 0 10px">
+                    <div class="display-flex">
+                        <p>Subtotal</p>
+                        <p>&#8369; 100.00</p>
+                    </div>
+                    <div class="display-flex">
+                        <p style="font-size: 18px;">Total</p>
+                        <p style="font-size: 18px;">&#8369; 200.00</p>
+                    </div>
+                </div>
+           </ion-card>
+            
         </ion-content>
+        
+        <ion-footer>
+            <ion-toolbar>
+                <ion-button>Review Payment</ion-button>
+            </ion-toolbar>
+        </ion-footer>
     </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage,  IonContent, IonCard, } from '@ionic/vue';
-import { arrowBackOutline } from 'ionicons/icons';
+import { IonPage, IonContent, IonCard, modalController,IonTextarea, IonLabel, IonItem, IonRadioGroup, IonRadio,
+    IonFooter} from '@ionic/vue';
+import { arrowBackOutline, mapOutline, pencilOutline, receiptOutline, walletOutline, bicycleOutline} from 'ionicons/icons';
 import { useRouter } from 'vue-router';
 import { defineComponent } from 'vue';
 import Header from '@/components/Header.vue';
+
+import Address from '@/components/Address.vue';
 export default defineComponent({
     name: 'Checkout',
-   
-    components: {IonContent, IonPage, IonCard, Header},
-    
- 
+    components: { IonContent, IonPage, IonCard, Header, IonTextarea, IonLabel, IonItem, 
+    IonFooter, IonRadioGroup, IonRadio},
     setup() {
-
-    const router = useRouter();
-    return { router, arrowBackOutline}
-  }
+        const router = useRouter();
+        return { router, arrowBackOutline, mapOutline, pencilOutline, receiptOutline, walletOutline, bicycleOutline}
+    },
+    methods: {
+        async addAddress() {
+        const modal = await modalController
+        .create({
+          component: Address,
+          cssClass: 'my-custom-class',
+          componentProps: {
+            title: 'Address'
+          },
+        })
+      return modal.present();
+    }
+    }
 })
 </script>
 
 <style scoped>
-
 ion-content {
-    --background: #eceff1;
+    --background: #fff;
 }
-.container {
-     background: url('/assets/images/bg-img/24.jpg') rgba(0,0,0,0.3);
-     background-blend-mode:darken; 
-     min-height: 200px; 
-     background-size:cover; 
-     background-position: center;
-     border-radius: 0 0 15% 15%; 
-     margin-top: -20%; 
-     padding-top: 20%;
+p {
+    font-weight: 600;
+     color: black;
 }
-h3.name-text {
-    color:#fff; 
-    text-shadow: 3px 3px 2px #000;
-    text-align:center;
-    font-size: 28px;
+p.address {
+    color: black;
+    text-align:Center;
+    font-size: 12px;
+    border: 1px solid rgba(0,0,0,0.06);
+    padding: 10px;
+    background: rgba(0,0,0,0.02);
+    box-shadow: 1px 1px 1px rgba(0,0,0,0.1);
+    border-radius: 5px;
 }
-.address-text {
-    font-size: 12px !important;
-    color: #fff;
-    text-align:center;
-    margin: auto 20px;
-    text-shadow: 1px 1px 1px #000;
-    border-radius: 15px;
-    border: 1px solid white;
-    padding: 5px;
+hr {
+    border-bottom: 1px solid rgba(0,0,0,0.1);
+    margin: 30px 0 0;
 }
-.store-hours {
-    font-size: 12px !important;
+
+ion-card {
+    padding: 10px 20px;
+    border-radius: 5px;
+    margin: 5px 10px 20px;
+    box-shadow: 1px 1px 5px rgba(0,0,0,0.2)
+}
+.title-icon {
+    display:flex; 
+    align-items:center; 
+    justify-content:space-around 
+}
+p.title {
+    margin: 10px 100px 10px 0px;
+    font-size: 16px;
+    color: black;
+}
+ion-icon.map {
+    font-size: 18px;
+    --color: black;
+}
+ion-icon.edit {
     color: #feb041;
-    text-align:center;
-    margin: auto 20px;
-    text-shadow: 1px 1px 1px #000;
-    padding: 5px;
+    font-size: 20px;
+}
+ion-label {
+    font-size: 16px !important;
+}
+ion-footer ion-toolbar {
+    --padding-top: 0;
+    --padding-bottom: 0;
+    --padding-start: 0;
+    --padding-end: 0;
+    --background: #ECEFF1;
+}
+ion-footer ion-button {
+    width: 100vw;
+    --border-radius: 0;
+    --color: #fff;
+    --margin: 0;
+}
+ion-radio-group  ion-label {
+    font-size: 16px !important;
+}
+.display-flex {
+    display:flex; 
+    justify-content:space-between
+}
+.summary-details {
+    padding: 10px 20px
 }
 </style>
